d697ce2f0f8216bcf4248793e3ea57de32702471
Juan Lucas <j.lucas.91@hotmail.com>
2022-11-07 11:01:48 +0100

    Estructura base

--- index.php
+++ index.php
@@ -1,6 +1,24 @@
 <?php
 
  // Carga de TWIG
-require_once 'vendor/autoload.php';
+require 'vendor/autoload.php';
 $loader = new \Twig\Loader\FilesystemLoader('templates');
 $twig = new \Twig\Environment($loader);
+
+// Carga de dependencias
+require 'configs/database.php';
+
+
+//Redirigir según el controlador solicitado y la acción
+if(isset($_GET['controller'])){
+    $controller = ucfirst($_GET['controller']).'Controller';
+    if(class_exists($controller)){
+    	$controller_object = new $controller();
+		if(isset($_GET['action'])){
+			$action = $_GET['action'];
+			$controller_object->$action();
+		}
+    }else{
+    }
+}else{
+}
